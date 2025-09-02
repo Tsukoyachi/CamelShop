46fe90c7cea1ea839bb99b956c0482e376d6a772
Axel Delille <Axel.delille31@gmail.com>
2025-09-02 14:30:14 +0200

    feat: bootstrap event-ingestion part

---
+++ event-ingestion/src/test/java/tsukoyachi/camelshop/eventingestion/EventIngestionApplicationTests.java
@@ -0,0 +1,13 @@
+package tsukoyachi.camelshop.eventingestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventIngestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
